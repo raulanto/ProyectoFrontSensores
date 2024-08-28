d3fd5be0ca1ea59d2eb95b0c976e8967164a7046
raul <74162376+raulanto@users.noreply.github.com>
2024-08-28 10:41:43 -0600

    useHead

--- app.vue
+++ app.vue
@@ -1,8 +1,35 @@
 <template>
   <div class="">
-    <darkmode/>
+
   </div>
 </template>
 <script setup lang="ts">
-import Darkmode from "~/components/global/darkmode.vue";
+useHead({
+    meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { key: 'theme-color', name: 'theme-color' }
+    ],
+    link: [
+        { rel: 'icon', href: '/logo.png' }
+    ],
+    htmlAttrs: {
+        lang: 'en'
+    }
+});
+
+const title = 'Panel Admin';
+const description = 'Sistema estadistico para sensores';
+
+useSeoMeta({
+    title,
+    description,
+    ogTitle: title,
+    ogDescription: description,
+    ogImage: 'https://dashboard-template.nuxt.dev/social-card.png',
+    // twitterImage: 'https://dashboard-template.nuxt.dev/social-card.png',
+    // twitterCard: 'summary_large_image'
+});
+
+
 </script>
