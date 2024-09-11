047e989821a09545d19291e73378537ca9a2c067
raul <74162376+raulanto@users.noreply.github.com>
2024-09-10 23:56:54 -0600

    cambios

--- composables/useEtapa.ts
+++ composables/useEtapa.ts
@@ -14,7 +14,30 @@ export function useEtapa() {
         }
         return data.value
     }
+    async function fetchEtapaid(id: number) {
+        const { data, error } = await useFetch(`http://127.0.0.1:8000/api/v1/etapa/?id=${id}`, {
+            headers: {
+                Authorization: `Token ${authStore.token}`,
+            },
+        });
+
+        if (error.value) {
+            throw new Error('Error al consumir la API');
+        }
+
+        // Extracting the 'fkequipo' from the first result in the response
+        const result = data.value?.results.[0].fkProceso.fkequipo;
+
+        if (!result) {
+            throw new Error('No fkequipo found in the response');
+        }
+
+        return result;
+    }
+
+
     return {
         fetchEtapa,
+        fetchEtapaid
     }
 }
--- pages/estado/[id].vue
+++ pages/estado/[id].vue
@@ -1,43 +1,48 @@
 <script setup>
-import {useRoute} from 'vue-router';
-import {computed, onBeforeMount, onMounted, ref} from 'vue'
-import {useProceso} from '~/composables/useProceso'
-
-
-const {fetchProceso} = useProceso()
-
-const proceso = ref([])
-
-
-onMounted(async () => {
-    try {
-        const data = await fetchProceso()
-        proceso.value = data.results
-    } catch (e) {
-        console.error('Error al obtener los datos:', e.message)
-    }
-})
-onBeforeMount(async () => {
-    try {
-        const data = await fetchProceso()
-        proceso.value = data.results
-    } catch (e) {
-        console.error('Error al obtener los datos:', e.message)
-    }
-})
+import { useRoute } from 'vue-router';
+import { computed, onMounted,onBeforeMount, ref } from 'vue';
+import { useProceso } from '~/composables/useProceso';
+import { useEtapa } from '~/composables/useEtapa';
+import TablaseccionEquipo from '~/components/equipo/tablaseccionEquipo.vue';
 
 const route = useRoute();
-
-const page = ref(1)
-const pageCount = 5
-
-const rows = computed(() => {
-    return proceso.value.slice((page.value - 1) * pageCount, (page.value) * pageCount)
-})
-
+const { fetchProceso } = useProceso();
+const { fetchEtapaid } = useEtapa();
 const id = ref(route.params.id);
+const proceso = ref([]);
+const etapa = ref([]);
+const fkequipo = ref()
+onMounted(async () => {
+    try {
+        const data = await fetchProceso();
+        const da = await fetchEtapaid(id.value);
+        etapa.value = da.results;
+        proceso.value = data.results;
+        // Accede al primer elemento de etapa y obtiene fkequiposs
 
-console.log('ID:', id);
+        fkequipo.value = etapa[0].fkProceso.fkequipo;
+    } catch (e) {
+        console.error('Error al obtener los datos:', e.message);
+    }
+});
+
+onBeforeMount(async () => {
+    try {
+        const data = await fetchProceso();
+        const da = await fetchEtapaid(id.value);
+        etapa.value = da.results;
+        proceso.value = data.results;
+        // Accede al primer elemento de etapa y obtiene fkequiposs
+
+        fkequipo.value = etapa.value;
+
+    } catch (e) {
+        console.error('Error al obtener los datos:', e.message);
+    }
+});
+
+console.log(fkequipo)
+
 
 
 definePageMeta({
@@ -45,35 +50,37 @@ definePageMeta({
     middleware: [
         'auth',
     ],
-
 });
 
-
-const links = [{
-    label: 'Proceso',
-    icon: 'i-heroicons-square-3-stack-3d',
-    to: '/procesos'
-},
+const links = [
     {
         label: 'Proceso',
         icon: 'i-heroicons-square-3-stack-3d',
         to: '/procesos'
-    }, {
+    },
+    {
         label: 'Panel del Proceso',
+        icon: 'i-heroicons-square-3-stack-3d',
+        to: { name: 'proceso-id', params: { id: id.value } }
+    },
+    {
+        label: 'Panel de la etapa',
         icon: 'i-heroicons-link'
-    }]
+    }
+];
 </script>
 
 <template>
-
-
     <section class="section-card">
-        <UBreadcrumb :links="links"/>
+        <UBreadcrumb :links="links" />
     </section>
 
+    <div class="grid grid-cols-5 grid-rows-1 gap-4">
+        <div class="col-span-4 section-card">
+            {{etapa}}
+        </div>
+        <div class="col-start-5 section-card">2</div>
+    </div>
 
+    <tablaseccion-equipo :fkequipo="fkequipo" />
 </template>
-
-<style scoped>
-
-</style>
