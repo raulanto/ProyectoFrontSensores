4e2450388023bd2548d32ba00be80507ade8bf42
raul <74162376+raulanto@users.noreply.github.com>
2024-11-30 12:39:36 -0600

    cambios

---
+++ server/api/login.ts
@@ -0,0 +1,13 @@
+
+
+export default defineEventHandler(
+    (event) => {
+    return {
+        "token": "386fc05a3228b0f4fcab67cb9bca9c0d13cb7c77",
+        "user_id": 1,
+        "email": "detretaty@gmail.com",
+        "first_name": "Raul antonio",
+        "groups": []
+    }
+}
+)
